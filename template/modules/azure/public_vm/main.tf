db3f32efd27626f50d5d18bcefb6a84ac9bc0bcd
Anna Szpoton <anna.szpoton@gmail.com>
2022-09-27 12:10:21 +0200

    Machine type

--- template/modules/azure/public_vm/main.tf
+++ template/modules/azure/public_vm/main.tf
@@ -87,7 +87,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     name                = "${var.prefix}-vm-${random_id.suffix.hex}"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    size                = "${var.machine_size}"
+    size                = "${var.machine_type}"
     admin_username      = "azureuser"
 
     network_interface_ids = [
